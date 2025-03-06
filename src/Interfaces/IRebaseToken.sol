463bb25d5e3ba4d7f4b4cf970f60117e5549cb15
Eunum56 <msiaffu123@gmail.com>
2025-03-06 20:25:33 +0530

    update interface

--- src/Interfaces/IRebaseToken.sol
+++ src/Interfaces/IRebaseToken.sol
@@ -2,7 +2,8 @@
 
 pragma solidity ^0.8.4;
 
-interface IRebaseToken{
+interface IRebaseToken {
     function mint(address to, uint256 amount) external;
     function burn(address from, uint256 amount) external;
+    function balanceOf(address account) external view returns (uint256);
 }
