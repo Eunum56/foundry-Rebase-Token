d432951e6e6aae57fd339a2ed4d5c1e54b1bf66b
Eunum56 <msiaffu123@gmail.com>
2025-03-15 20:06:47 +0530

    Update interface

--- src/Interfaces/IRebaseToken.sol
+++ src/Interfaces/IRebaseToken.sol
@@ -6,6 +6,6 @@ interface IRebaseToken {
     function mint(address to, uint256 amount, uint256 interestRate) external;
     function burn(address from, uint256 amount) external;
     function balanceOf(address account) external view returns (uint256);
-    function getUserInterestRate(address user) external view returns(uint256);
-    function getInterestRate() external view returns(uint256);
+    function getUserInterestRate(address user) external view returns (uint256);
+    function getInterestRate() external view returns (uint256);
 }
